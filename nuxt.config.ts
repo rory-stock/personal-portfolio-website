39c5ac0e97e9998002a8efeae20be07dc32ab3b1
Rory <rorystock06@gmail.com>
2024-12-19 12:47:57 +1300

    Reapply "feat: start add images to homepage"
    This reverts commit 020957b960b91b22af7a99d111d72e441e1920d4.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
+  ssr: false,
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  image: {
+    formats: ["webp", "jpeg"],
+  },
+
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     head: {
@@ -87,4 +95,6 @@ export default defineNuxtConfig({
       ],
     },
   },
+
+  modules: ["@nuxt/image"],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,38 @@
 useHead({
   title: "Home",
 });
+
+const images = [
+  {
+    src: "/images/4.jpg",
+    alt: "Person digging bike track",
+  },
+  {
+    src: "/images/Digital.jpg",
+    alt: "Close up portrait of mountain biker",
+  },
+  {
+    src: "/images/P1003670.jpg",
+    alt: "Mountain biker riding off large drop",
+  },
+  {
+    src: "/images/P1027141.jpg",
+    alt: "Mountain biker riding off large drop",
+  },
+];
 </script>
 
 <template>
-  <div></div>
+  <div class="flex flex-col md:hidden gap-4 pl-4 pr-4 pt-4 pb-4">
+    <div v-for="image in images" :key="image.src">
+      <NuxtPicture
+        :src="image.src"
+        :alt="image.alt"
+        loading="lazy"
+        class="w-auto"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
