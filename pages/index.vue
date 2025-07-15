0ba2f234dc7d0e0ddaaeea4538be83aade252099
Rory <rorystock06@gmail.com>
2025-07-15 13:06:32 +0100

    feat: add new images to home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ useHead({
 });
 
 const images = [
-  // ----------------- ROW 1 ----------------- //
+  // ----------------- ROW ----------------- //
   {
     src: "/home/R5RS6542-2.jpg",
     alt: "Mountain biker doing a jump",
@@ -18,7 +18,21 @@ const images = [
     alt: "Mountain biker doing a jump",
   },
   // ----------------------------------------- //
-  // ----------------- ROW 2 ----------------- //
+  // ----------------- ROW ----------------- //
+  {
+    src: "/home/R5RS9096.jpg",
+    alt: "Rider cornering with dust trail",
+  },
+  {
+    src: "/home/R5RS3890.jpg",
+    alt: "Jackson Goldstone celebrating a win",
+  },
+  {
+    src: "/home/R5RS6835.jpg",
+    alt: "Two riders going round a dusty corner",
+  },
+
+  // ----------------- ROW ----------------- //
   {
     src: "/home/R5RS0282.jpg",
     alt: "Close up portrait of mountain biker",
@@ -32,7 +46,7 @@ const images = [
     alt: "Close up of a rider going round a flat corner",
   },
   // ----------------------------------------- //
-  // ----------------- ROW 3 ----------------- //
+  // ----------------- ROW ----------------- //
   {
     src: "/home/R5RS4616.jpg",
     alt: "Close up portrait of mountain biker using a slow shutter speed",
@@ -46,7 +60,7 @@ const images = [
     alt: "Close up portrait of Jackson Goldstone celebrating a win",
   },
   // ----------------------------------------- //
-  // ----------------- ROW 4 ----------------- //
+  // ----------------- ROW ----------------- //
   {
     src: "/home/R5RS8637.jpg",
     alt: "Portrait of Lachie Stevens-Mcnab at a mountain bike event",
@@ -60,7 +74,7 @@ const images = [
     alt: "Mountain biker doing a jump",
   },
   // ----------------------------------------- //
-  // ----------------- ROW 5 ----------------- //
+  // ----------------- ROW ----------------- //
   {
     src: "/home/R5RS5760.jpg",
     alt: "Mountain biker doing a jump",
